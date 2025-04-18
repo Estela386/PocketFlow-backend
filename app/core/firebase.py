4f3d40628d167b1e47e8c83ef8de568c1f717fce
Nitban <tiggertok@gmail.com>
2025-04-17 23:15:27 -0600

    ruta de las credenciales corregida

--- app/core/firebase.py
+++ app/core/firebase.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("path/to/cred.json")
+cred = credentials.Certificate("pocket-flow-827cf-firebase-adminsdk-fbsvc-aaa30269cf.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
