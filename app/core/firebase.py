7c1d3ca1bb6b216be3e95f330c38fde30f903ab7
Nitban <tiggertok@gmail.com>
2025-04-17 23:08:26 -0600

    Conectado a Firebase

--- app/core/firebase.py
+++ app/core/firebase.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("path/to/cred.json")
+cred = credentials.Certificate("ruta/tu/firebase_credentials.json")
 firebase_admin.initialize_app(cred)
+
 db = firestore.client()
